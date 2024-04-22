40bc204050223bfbcf28e3988f4b55b502d61489
Miriam Janet <mjgs29091998@gmail.com>
2024-04-22 11:47:23 -0600

    Segundo día de entrenamiento

--- Ejemplos/Variables.py
+++ Ejemplos/Variables.py
@@ -16,4 +16,11 @@ print("Entonces el siguiente año tu edad será de " + new_edad)
 """
 usando la forma de formatear cadena
 """
+color_carro = input("Cuál es el color de tu carro = ")
+matricula = input("Cuál es tu matricula = ")
+pago = input("Cuanto vas a pagar = ")
 
+multa = 452
+pago_multa = multa * 2
+
+print(f"Tu carro es de color {color_carro} con matricula {matricula}, por lo que tu pago de multa es {pago_multa}")
