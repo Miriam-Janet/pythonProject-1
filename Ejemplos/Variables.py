f396f42e84d1a62eed172cf0aec807a26b7f31d4
Miriam Janet <mjgs29091998@gmail.com>
2024-04-22 11:37:27 -0600

    Segundo día de entrenamiento

---
+++ Ejemplos/Variables.py
@@ -0,0 +1,18 @@
+tu_edad = input("Dime tu edad = ")
+
+print("Tu edad es " + tu_edad)
+print((type(tu_edad)))
+
+tu_edad = int(tu_edad)
+
+print((type(tu_edad)))
+
+new_edad = 1 + tu_edad
+
+new_edad = str(new_edad)
+
+print("Entonces el siguiente año tu edad será de " + new_edad)
+
+"""
+usando la forma de formatear cadena
+"""
