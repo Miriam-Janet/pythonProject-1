a2e1c1859ed13f97d4d933eb4a9c2e6da64272a1
Miriam Janet <mjgs29091998@gmail.com>
2024-04-22 11:32:00 -0600

    Initial commit

---
+++ Ejemplos/First.py
@@ -0,0 +1,8 @@
+#print - Es mostrar en pantalla
+
+print("Hola mundo, estoy aprendiendo en 2024 python")
+
+print("Tu te llamas " + input("Comó te llamas? = ") + "\nY tienes " + input("Cuantos años tienes? = "))
+
+print("Estas estudiando " + input("¿Qué estas estudiando?"))
+
