df9b55b10b77b7d235149517e36734795b26d84f
Miriam Janet <mjgs29091998@gmail.com>
2025-01-22 09:23:51 -0600

    Tercer día de entrenamiento Tecnm

---
+++ Curso/Input-practicas.py
@@ -0,0 +1,12 @@
+"""
+Esta es parte de una tarea.
+Clase, es un plano donde se puede construri la información necesaria, para crear lo que son nuevas intancias, lo que son los objetos, los cuales estas
+variables tiene un tipo de dato especifico, donde son los objetos, y estos mismos objetos tienen lo que son los atributos y métodos necesarios para
+realizar lo que son las operaciones necesarias para resolver un problema.
+"""
+
+"""Proyecto a realizar núm. 2
+"""
+
+texto = input("Ingresa una oración que contenga "")
+
